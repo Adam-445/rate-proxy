80a00e4b859c703ee84d4feef607228c29260157
Adam-445 <Adamch012016@outlook.com>
2026-02-15 13:36:06 +0000

    test: add TestLimiter_RefillsTokensOverTime to verify token refill

--- cmd/proxy/limiter_test.go
+++ cmd/proxy/limiter_test.go
@@ -6,10 +6,9 @@ import (
 )
 
 func TestLimiter_AllowsRequestsUpToCapacity(t *testing.T) {
-	fixedTime := time.Unix(0, 0).UTC()
-
 	store := &InMemroyBucketStore{data: make(map[string]*bucket)}
 	limiter := NewLimiter(store, 3, 1)
+	fixedTime := time.Unix(0, 0).UTC()
 
 	for i := range 4 {
 		allowed := limiter.Allow("TestClient", fixedTime)
@@ -19,5 +18,35 @@ func TestLimiter_AllowsRequestsUpToCapacity(t *testing.T) {
 		if i >= 3 && allowed {
 			t.Errorf("expected request %d to be denied", i)
 		}
+	}
+}
+
+func TestLimiter_RefillsTokensOverTime(t *testing.T) {
+	store := &InMemroyBucketStore{data: make(map[string]*bucket)}
+	limiter := NewLimiter(store, 3, 1)
+	fixedTime := time.Unix(0, 0).UTC()
+	requestNumber := 0
+
+	for ; requestNumber < 3; requestNumber++ {
+		allowed := limiter.Allow("TestClient", fixedTime)
+
+		if !allowed {
+			t.Errorf("expected request %d to be allowed", requestNumber)
+		}
+	}
+
+	fixedTime = fixedTime.Add(2 * time.Second)
+	for ; requestNumber < 5; requestNumber++ {
+		allowed := limiter.Allow("TestClient", fixedTime)
+
+		if !allowed {
+			t.Errorf("expected request %d to be allowed", requestNumber)
+		}
+	}
+
+	allowed := limiter.Allow("TestClient", fixedTime)
+	requestNumber++
+	if allowed {
+		t.Errorf("expected request %d to be denied", requestNumber)
 	}
 }
