82da306090f6a98af7f0f6b11f078339b066bd78
Adam-445 <Adamch012016@outlook.com>
2026-02-15 08:54:35 +0000

    feat: encapsulate limiter logic

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -10,8 +10,6 @@ import (
 	"time"
 )
 
-var buckets sync.Map
-
 const (
 	MaxCapacity     float64 = 10
 	TokensPerSecond float64 = 1
@@ -23,6 +21,44 @@ type bucket struct {
 	mu        sync.Mutex
 }
 
+type limiter struct {
+	buckets  map[string]*bucket // TODO: Add bucket cleanup (TTL, periodic cleanup, LRU)
+	mu       sync.Mutex
+	capacity float64
+	rate     float64
+}
+
+func NewLimiter(maxCapacity float64, tokensPerSecond float64) *limiter {
+	return &limiter{buckets: make(map[string]*bucket), capacity: maxCapacity, rate: tokensPerSecond}
+}
+
+func (l *limiter) Allow(clientID string, now time.Time) bool {
+	// Ckeck if a bucket for the current user exists
+	_, ok := l.buckets[clientID]
+	if !ok {
+		l.buckets[clientID] = &bucket{tokens: l.capacity, timestamp: now}
+	}
+	userBucket := l.buckets[clientID]
+
+	userBucket.mu.Lock()
+	defer userBucket.mu.Unlock()
+
+	// Refill
+	elapsed := time.Since(userBucket.timestamp).Seconds()
+
+	userBucket.tokens = math.Min(l.capacity, userBucket.tokens+(elapsed*l.rate))
+	userBucket.timestamp = now
+
+	// Consume
+	if userBucket.tokens >= 1 {
+		userBucket.tokens -= 1
+	} else {
+		return false
+	}
+
+	return true
+}
+
 func main() {
 	// Use httputil.ReverseProxy to forward requests from client to process / server
 	target := url.URL{
@@ -30,6 +66,7 @@ func main() {
 		Host:   "localhost:8081",
 	}
 	proxy := httputil.NewSingleHostReverseProxy(&target)
+	limiter := NewLimiter(10, 1)
 
 	// Create a handler that wraps the proxy
 	handler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
@@ -37,30 +74,7 @@ func main() {
 		if err != nil {
 			host = req.RemoteAddr
 		}
-
-		now := time.Now()
-		// Check if a bucket for the current user exists
-		actual, _ := buckets.LoadOrStore(host, &bucket{tokens: MaxCapacity, timestamp: now})
-
-		userBucket, ok := actual.(*bucket)
-		if !ok {
-			http.Error(rw, "Internal server error", http.StatusInternalServerError)
-			return
-		}
-
-		userBucket.mu.Lock()
-		defer userBucket.mu.Unlock()
-
-		// Refill
-		elapsed := time.Since(userBucket.timestamp).Seconds()
-
-		userBucket.tokens = math.Min(MaxCapacity, userBucket.tokens+(elapsed*TokensPerSecond))
-		userBucket.timestamp = now
-
-		// Consume
-		if userBucket.tokens >= 1 {
-			userBucket.tokens -= 1
-		} else {
+		if !limiter.Allow(host, time.Now()) {
 			http.Error(rw, "Request limit reached", http.StatusTooManyRequests)
 			return
 		}
