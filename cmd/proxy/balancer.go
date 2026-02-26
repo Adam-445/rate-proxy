72a8054924a2216620b4b301b958ce6c4e5f6b47
Adam-445 <Adamch012016@outlook.com>
2026-02-26 15:15:42 +0000

    feat: add backend as its own type

--- cmd/proxy/balancer.go
+++ cmd/proxy/balancer.go
@@ -7,9 +7,14 @@ import (
 	"sync/atomic"
 )
 
+type backend struct {
+	address string
+	flag    bool
+}
+
 type Balancer struct {
 	// TODO: skip dead backends / healthchecks
-	backends []string // List of backend addresses
+	backends []*backend // List of backend addresses
 	// TODO: Consider making balancing algorithms swappable
 	counter atomic.Uint32 // Thread-safe counter
 	// TODO: Replace permanent caching to face changes
@@ -21,13 +26,20 @@ type Balancer struct {
 }
 
 func NewBalancer(backendAdresses []string) *Balancer {
-	return &Balancer{backends: backendAdresses}
+	backends := make([]*backend, len(backendAdresses))
+	for i, addr := range backendAdresses {
+		backends[i] = &backend{
+			address: addr,
+			flag:    false,
+		}
+	}
+	return &Balancer{backends: backends}
 }
 
 func (b *Balancer) GetNextBackend() string {
 	n := b.counter.Add(1)
 	idx := int(n-1) % len(b.backends)
-	return b.backends[idx]
+	return b.backends[idx].address
 }
 
 func (b *Balancer) GetProxy(target *url.URL) *httputil.ReverseProxy {
