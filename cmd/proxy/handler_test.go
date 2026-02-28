df77009a58014400be89f0982c30f6f03a696716
Adam-445 <Adamch012016@outlook.com>
2026-02-28 08:59:30 +0000

    test: add TestHandler_NormalRequest for testing normal handling of requests

---
+++ cmd/proxy/handler_test.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"net/url"
+	"testing"
+	"time"
+)
+
+type (
+	FakeLimiter struct {
+		allow bool
+	}
+	FakeBalancer struct {
+		backendAddress string
+		error          error
+	}
+)
+
+func (l *FakeLimiter) Allow(clientID string, now time.Time) bool {
+	return l.allow
+}
+
+func (b *FakeBalancer) GetNextBackend() (string, error) {
+	return b.backendAddress, b.error
+}
+
+func (b *FakeBalancer) GetProxy(target *url.URL) *httputil.ReverseProxy {
+	proxy := httputil.NewSingleHostReverseProxy(target)
+	return proxy
+}
+
+func TestHandler_NormalRequest(t *testing.T) {
+	// Normal request -> expected to be handled correctly
+	expected := "Hello"
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if _, err := fmt.Fprint(w, expected); err != nil {
+			t.Fatalf("error writing response: %s", err)
+		}
+	}))
+	defer svr.Close()
+
+	limiter := &FakeLimiter{allow: true}
+	balancer := &FakeBalancer{backendAddress: svr.URL, error: nil}
+	logger := slog.New(slog.NewTextHandler(io.Discard, nil))
+	handler := NewProxyHandler(limiter, balancer, logger)
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	recorder := httptest.NewRecorder()
+	handler.ServeHTTP(recorder, req)
+
+	res := recorder.Result()
+	defer func() { _ = res.Body.Close() }()
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil. got %v", err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Status code error. got %d, want %d", res.StatusCode, http.StatusOK)
+	}
+	if string(data) != expected {
+		t.Errorf("Result body mismatch. got %s, want %s", string(data), expected)
+	}
+}
+
+func TestHandler_RateLimiting(t *testing.T) {
+}
+
+func TestHandler_NoBackendsAvailable(t *testing.T) {
+}
