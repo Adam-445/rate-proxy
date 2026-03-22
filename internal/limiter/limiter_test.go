f3b0c76537a1c991f9827f327a28f95e7abad0f0
Adam-445 <Adamch012016@outlook.com>
2026-03-22 20:29:58 +0100

    tests(limiter): add TestConcurrency test

--- internal/limiter/limiter_test.go
+++ internal/limiter/limiter_test.go
@@ -1,17 +1,20 @@
 package limiter
 
 import (
+	"context"
+	"sync"
 	"testing"
 	"time"
 )
 
 func TestAllowsRequestsUpToCapacity(t *testing.T) {
+	ctx := context.Background()
 	store := &InMemoryBucketStore{data: make(map[string]*bucket)}
 	limiter := NewLimiter(store, 3, 1)
 	fixedTime := time.Unix(0, 0).UTC()
 
 	for i := range 4 {
-		allowed, err := limiter.Allow("TestClient", fixedTime)
+		allowed, err := limiter.Allow(ctx, "TestClient", fixedTime)
 		if err != nil {
 			t.Fatalf("unexpected error from limiter: %v", err)
 		}
@@ -25,13 +28,14 @@ func TestAllowsRequestsUpToCapacity(t *testing.T) {
 }
 
 func TestRefillsTokensOverTime(t *testing.T) {
+	ctx := context.Background()
 	store := &InMemoryBucketStore{data: make(map[string]*bucket)}
 	limiter := NewLimiter(store, 3, 1)
 	fixedTime := time.Unix(0, 0).UTC()
 	requestNumber := 0
 
 	for ; requestNumber < 3; requestNumber++ {
-		allowed, err := limiter.Allow("TestClient", fixedTime)
+		allowed, err := limiter.Allow(ctx, "TestClient", fixedTime)
 		if err != nil {
 			t.Fatalf("unexpected error from limiter: %v", err)
 		}
@@ -43,7 +47,7 @@ func TestRefillsTokensOverTime(t *testing.T) {
 
 	fixedTime = fixedTime.Add(2 * time.Second)
 	for ; requestNumber < 5; requestNumber++ {
-		allowed, err := limiter.Allow("TestClient", fixedTime)
+		allowed, err := limiter.Allow(ctx, "TestClient", fixedTime)
 		if err != nil {
 			t.Fatalf("unexpected error from limiter: %v", err)
 		}
@@ -53,12 +57,51 @@ func TestRefillsTokensOverTime(t *testing.T) {
 		}
 	}
 
-	allowed, err := limiter.Allow("TestClient", fixedTime)
+	allowed, err := limiter.Allow(ctx, "TestClient", fixedTime)
 	if err != nil {
 		t.Fatalf("unexpected error from limiter: %v", err)
 	}
 	requestNumber++
 	if allowed {
 		t.Errorf("expected request %d to be denied", requestNumber)
+	}
+}
+
+func TestConcurrency(t *testing.T) {
+	ctx := context.Background()
+	store := NewInMemoryBucketStore()
+
+	bucketCapacity := 900
+	l := NewLimiter(store, float64(bucketCapacity), 1)
+
+	clientID := "concurrent-client"
+	fixedTime := time.Unix(0, 0).UTC()
+
+	var wg sync.WaitGroup
+	requestCount := 1000
+	results := make(chan bool, requestCount)
+
+	// Fire requests simultaneously
+	for range requestCount {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			allowed, _ := l.Allow(ctx, clientID, fixedTime)
+			results <- allowed
+		}()
+	}
+
+	wg.Wait()
+	close(results)
+
+	allowedCount := 0
+	for res := range results {
+		if res {
+			allowedCount++
+		}
+	}
+
+	if allowedCount != bucketCapacity {
+		t.Errorf("expected %d allowed requests under concurrency, got %d", bucketCapacity, allowedCount)
 	}
 }
